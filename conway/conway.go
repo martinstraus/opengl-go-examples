2b60ed8cae0b02d14ea61ce68e5bb9f16de59da6
Martin Straus <martinstraus@gmail.com>
2021-03-14 23:02:35 -0300

    Added game logic.

--- conway/conway.go
+++ conway/conway.go
@@ -6,8 +6,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"runtime"
 	"strings"
+	"time"
 
 	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -32,18 +34,27 @@ const (
     }
 ` + "\x00"
 
-	rows    = 10
-	columns = 10
+	rows      = 100
+	columns   = 100
+	threshold = 0.15
+	fps       = 5
 )
 
 type cell struct {
 	drawable uint32
+
+	alive     bool
+	aliveNext bool
 
 	x int
 	y int
 }
 
 func (c *cell) draw() {
+	if !c.alive {
+		return
+	}
+
 	gl.BindVertexArray(c.drawable)
 	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(square)/3))
 }
@@ -71,7 +82,17 @@ func main() {
 	cells := makeCells()
 
 	for !window.ShouldClose() {
+		t := time.Now()
+
+		for x := range cells {
+			for _, c := range cells[x] {
+				c.checkState(cells)
+			}
+		}
+
 		draw(cells, window, program)
+
+		time.Sleep(time.Second/time.Duration(fps) - time.Since(t))
 	}
 }
 
@@ -175,10 +196,13 @@ func compileShader(source string, shaderType uint32) (uint32, error) {
 }
 
 func makeCells() [][]*cell {
+	rand.Seed(time.Now().UnixNano())
 	cells := make([][]*cell, rows, rows)
 	for x := 0; x < rows; x++ {
 		for y := 0; y < columns; y++ {
 			c := newCell(x, y)
+			c.alive = rand.Float64() < threshold
+			c.aliveNext = c.alive
 			cells[x] = append(cells[x], c)
 		}
 	}
@@ -217,4 +241,66 @@ func newCell(x, y int) *cell {
 		x: x,
 		y: y,
 	}
+}
+
+// checkState determines the state of the cell for the next tick of the game.
+func (c *cell) checkState(cells [][]*cell) {
+	c.alive = c.aliveNext
+	c.aliveNext = c.alive
+
+	liveCount := c.liveNeighbors(cells)
+	if c.alive {
+		// 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+		if liveCount < 2 {
+			c.aliveNext = false
+		}
+
+		// 2. Any live cell with two or three live neighbours lives on to the next generation.
+		if liveCount == 2 || liveCount == 3 {
+			c.aliveNext = true
+		}
+
+		// 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+		if liveCount > 3 {
+			c.aliveNext = false
+		}
+	} else {
+		// 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+		if liveCount == 3 {
+			c.aliveNext = true
+		}
+	}
+}
+
+// liveNeighbors returns the number of live neighbors for a cell.
+func (c *cell) liveNeighbors(cells [][]*cell) int {
+	var liveCount int
+	add := func(x, y int) {
+		// If we're at an edge, check the other side of the board.
+		if x == len(cells) {
+			x = 0
+		} else if x == -1 {
+			x = len(cells) - 1
+		}
+		if y == len(cells[x]) {
+			y = 0
+		} else if y == -1 {
+			y = len(cells[x]) - 1
+		}
+
+		if cells[x][y].alive {
+			liveCount++
+		}
+	}
+
+	add(c.x-1, c.y)   // To the left
+	add(c.x+1, c.y)   // To the right
+	add(c.x, c.y+1)   // up
+	add(c.x, c.y-1)   // down
+	add(c.x-1, c.y+1) // top-left
+	add(c.x+1, c.y+1) // top-right
+	add(c.x-1, c.y-1) // bottom-left
+	add(c.x+1, c.y-1) // bottom-right
+
+	return liveCount
 }
